b4b044438558ab21df0c66ec6d02220185f11923
Tristin <charismental@gmail.com>
2019-11-07 14:59:13 -0800

    Added Side Navigation

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,39 @@
 <template>
-  <v-app>
+  <v-app style="background = #E3E3EE">
+    <!-- Side Navbar -->
+    <v-navigation-drawer app temporary fixed v-model="sideNav">
+      <v-toolbar color="accent" dark text>
+        <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
+        <router-link to="/" tag="span" style="cursor: pointer">
+          <h1 class="title pl-3">VueShare</h1>
+        </router-link>
+      </v-toolbar>
+
+      <v-divider></v-divider>
+
+      <!-- Side Navbar Links -->
+      <v-list>
+        <v-list-item
+          ripple
+          v-for="(item, i) in sideNavItems"
+          :key="i"
+          :to="item.link"
+        >
+          <v-list-item-action>
+            <v-icon>
+              {{ item.icon }}
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            {{ item.title }}
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <!-- Horizontal Navbar -->
     <v-app-bar color="primary" dark app>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
       <v-toolbar-title class="hidden-xs-only">
         <router-link to="/" tag="span" style="cursor: pointer">
           VueShare
@@ -55,10 +86,24 @@ export default {
         { icon: 'lock_open', title: 'Sign In', link: '/signin' },
         { icon: 'create', title: 'Sign Up', link: '/signup' }
       ]
+    },
+    sideNavItems() {
+      return [
+        { icon: 'chat', title: 'Posts', link: '/posts' },
+        { icon: 'lock_open', title: 'Sign In', link: '/signin' },
+        { icon: 'create', title: 'Sign Up', link: '/signup' }
+      ]
     }
   },
   data() {
-    return {}
+    return {
+      sideNav: false
+    }
+  },
+  methods: {
+    toggleSideNav() {
+      this.sideNav = !this.sideNav
+    }
   }
 }
 </script>
