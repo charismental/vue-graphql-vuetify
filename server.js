f33dd9635ad27349e34f66b7b1c2c3b8b773da4a
Tristin <charismental@gmail.com>
2019-11-07 10:03:37 -0800

    Created models and typedefs

---
+++ server.js
@@ -0,0 +1,36 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname, "typeDefs.gql");
+const typeDefs = fs.readFileSync(filePath, "utf-8");
+const resolvers = require("./resolvers");
+
+require("dotenv").config({ path: "variables.env" });
+const User = require("./models/User");
+const Post = require("./models/Post");
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch(err => console.error(err));
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: {
+    User,
+    Post
+  }
+});
+
+server.listen(4500).then(({ url }) => {
+  console.log(`Server listening on ${url}`);
+});
