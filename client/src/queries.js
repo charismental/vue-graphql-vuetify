b074ba9f4afbd01f86383e395404d4cab12393e4
Tristin <charismental@gmail.com>
2019-11-12 15:49:31 -0800

    get current user query / resolver

--- client/src/queries.js
+++ client/src/queries.js
@@ -1,7 +1,6 @@
 import { gql } from 'apollo-boost'
 
 // Post Queries
-
 export const GET_POSTS = gql`
   query {
     getPosts {
@@ -12,7 +11,40 @@ export const GET_POSTS = gql`
   }
 `
 // User Queries
+export const GET_CURRENT_USER = gql`
+  query {
+    getCurrentUser {
+      _id
+      username
+      email
+      password
+      avatar
+      joinDate
+      favorites {
+        _id
+        title
+        imageUrl
+      }
+    }
+  }
+`
+
 
 // Posts Mutations
 
 // User Mutations
+export const SIGNIN_USER = gql`
+  mutation($username: String!, $password: String!) {
+    signinUser(username: $username, password: $password) {
+      token
+    }
+  }
+`
+
+export const SIGNUP_USER = gql`
+  mutation($username: String!, $email: String!, $password: String!) {
+    signupUser(username: $username, email: $email, password: $password) {
+      token
+    }
+  }
+`
