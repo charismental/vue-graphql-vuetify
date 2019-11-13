d90762bc10bce77e6035b01254909d6e19a4973b
Tristin <charismental@gmail.com>
2019-11-12 19:13:30 -0800

    Setup signup page

--- client/src/components/Auth/Signup.vue
+++ client/src/components/Auth/Signup.vue
@@ -1,11 +1,198 @@
 <template>
-  <v-container>
-    <h1>Signup</h1>
+  <v-container class="text-center mt-4 pt-4">
+    <v-row>
+      <v-col sm="6" offset-sm="3">
+        <h1>Get Started Here</h1>
+      </v-col>
+    </v-row>
+
+    <!-- Error alert -->
+    <v-row v-if="error">
+      <v-col sm="6" offset-sm="3" class="mb-n8">
+        <form-alert :message="error.message"></form-alert>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col sm="6" offset-sm="3">
+        <v-card color="accent" dark>
+          <v-container>
+            <v-form
+              v-model="isFormValid"
+              lazy-validation
+              ref="form"
+              @submit.prevent="handleSignupUser"
+            >
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    :rules="usernameRules"
+                    v-model="username"
+                    prepend-icon="face"
+                    placeholder="Username"
+                    type="text"
+                    required
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    :rules="emailRules"
+                    v-model="email"
+                    prepend-icon="email"
+                    placeholder="Email"
+                    type="email"
+                    required
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    :rules="passwordRules"
+                    v-model="password"
+                    prepend-icon="extension"
+                    placeholder="Password"
+                    type="password"
+                    required
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    :rules="passwordRules"
+                    v-model="passwordConfirmation"
+                    prepend-icon="gavel"
+                    placeholder="Confirm password"
+                    type="password"
+                    required
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+
+              <v-row>
+                <v-col>
+                  <v-btn
+                    :loading="loading"
+                    :disabled="!isFormValid || loading"
+                    color="info"
+                    type="submit"
+                  >
+                    <template v-slot:loader>
+                      <span class="custom-loader">
+                        <v-icon light>cached</v-icon>
+                      </span>
+                    </template>
+                    Signup</v-btn
+                  >
+                  <h3>
+                    Already have an account?
+                    <router-link to="/signin">Signin</router-link>
+                  </h3>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-container>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  name: 'signup'
+  name: 'Signup',
+  data() {
+    return {
+      isFormValid: true,
+      username: '',
+      password: '',
+      passwordConfirmation: '',
+      email: '',
+      emailRules: [
+        email => !!email || 'Email is required',
+        email => /.@+./.test(email) || 'Email must be valid'
+      ],
+      usernameRules: [
+        username => !!username || 'Username is required',
+        username =>
+          username.length < 10 || 'Username cannot be more than 10 characters'
+      ],
+      passwordRules: [
+        password => !!password || 'Password is required',
+        password =>
+          password.length >= 6 || 'Password must be at least 6 characters',
+        confirmation => confirmation === this.password || 'Passwords must match'
+      ]
+    }
+  },
+  computed: {
+    ...mapGetters(['user', 'error', 'loading'])
+  },
+  watch: {
+    user(value) {
+      // if user value changes, redirect to home page
+      if (value) {
+        this.$router.push('/')
+      }
+    }
+  },
+  methods: {
+    handleSignupUser() {
+      if (this.$refs.form.validate()) {
+        this.$store.dispatch('signupUser', {
+          username: this.username,
+          email: this.email,
+          password: this.password
+        })
+      }
+    }
+  }
 }
 </script>
+
+<style>
+.custom-loader {
+  animation: loader 1s infinite;
+  display: flex;
+}
+@-moz-keyframes loader {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+@-webkit-keyframes loader {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+@-o-keyframes loader {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+@keyframes loader {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
