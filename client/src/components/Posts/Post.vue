ea577e342db467e6b38036fb16fcebfe11b55f7d
Tristin <charismental@gmail.com>
2019-11-14 14:34:39 -0800

    Handle likes/unlikes

--- client/src/components/Posts/Post.vue
+++ client/src/components/Posts/Post.vue
@@ -6,8 +6,12 @@
         <v-card hover>
           <v-card-title>
             <h1>{{ getPost.title }}</h1>
-            <v-btn large icon v-if="!user">
-              <v-icon large color="grey">favorite</v-icon>
+            <v-btn @click="handleToggleLike" large icon v-if="user">
+              <v-icon
+                large
+                :color="checkIfPostLiked(getPost._id) ? 'red' : 'grey'"
+                >favorite</v-icon
+              >
             </v-btn>
             <h3 class="ml-3 font-weight-thin">{{ getPost.likes }} LIKES</h3>
             <v-spacer></v-spacer>
@@ -115,7 +119,12 @@
 </template>
 
 <script>
-import { GET_POST, ADD_POST_MESSAGE } from '../../queries.js'
+import {
+  GET_POST,
+  ADD_POST_MESSAGE,
+  LIKE_POST,
+  UNLIKE_POST
+} from '../../queries.js'
 import { mapGetters } from 'vuex'
 
 export default {
@@ -123,6 +132,7 @@ export default {
   props: ['postId'],
   data() {
     return {
+      postLiked: false,
       dialog: false,
       messageBody: '',
       isFormValid: true,
@@ -144,9 +154,99 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(['user'])
+    ...mapGetters(['user', 'userFavorites'])
   },
   methods: {
+    checkIfPostLiked(postId) {
+      // check if user favorites includes post with id of 'postId'
+      if (
+        this.userFavorites &&
+        this.userFavorites.some(fave => fave._id === postId)
+      ) {
+        this.postLiked = true
+        return true
+      } else {
+        this.postLiked = false
+        return false
+      }
+    },
+    handleToggleLike() {
+      if (this.postLiked) {
+        this.handleUnlikePost()
+      } else {
+        this.handleLikePost()
+      }
+    },
+    handleLikePost() {
+      const variables = {
+        postId: this.postId,
+        username: this.user.username
+      }
+      this.$apollo
+        .mutate({
+          mutation: LIKE_POST,
+          variables,
+          // eslint-disable-next-line no-unused-vars
+          update: (cache, { data: { likePost } }) => {
+            const data = cache.readQuery({
+              query: GET_POST,
+              variables: { postId: this.postId }
+            })
+            data.getPost.likes += 1
+            cache.writeQuery({
+              query: GET_POST,
+              variables: { postId: this.postId },
+              data
+            })
+          }
+        })
+        .then(({ data }) => {
+          const updatedUser = {
+            ...this.user,
+            favorites: data.likePost.favorites
+          }
+          this.$store.commit('SET_USER', updatedUser)
+        })
+        .catch(err => {
+          // eslint-disable-next-line no-console
+          console.error(err)
+        })
+    },
+    handleUnlikePost() {
+      const variables = {
+        postId: this.postId,
+        username: this.user.username
+      }
+      this.$apollo
+        .mutate({
+          mutation: UNLIKE_POST,
+          variables,
+          // eslint-disable-next-line no-unused-vars
+          update: (cache, { data: { unlikePost } }) => {
+            const data = cache.readQuery({
+              query: GET_POST,
+              variables: { postId: this.postId }
+            })
+            data.getPost.likes -= 1
+            cache.writeQuery({
+              query: GET_POST,
+              variables: { postId: this.postId },
+              data
+            })
+          }
+        })
+        .then(({ data }) => {
+          const updatedUser = {
+            ...this.user,
+            favorites: data.unlikePost.favorites
+          }
+          this.$store.commit('SET_USER', updatedUser)
+        })
+        .catch(err => {
+          // eslint-disable-next-line no-console
+          console.error(err)
+        })
+    },
     checkIfOwnMessage(message) {
       return this.user && this.user._id === message.messageUser._id
     },
